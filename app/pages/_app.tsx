bcd0d2824ebc6ffc532ca26414aad657fb0f9c86
0ptocat <149520335+0ptocat@users.noreply.github.com>
2023-11-26 17:45:09 +0000

    test fix 3

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,28 +1,38 @@
-import { AppType } from "next/dist/shared/lib/utils";
-import { withTRPC } from "@trpc/next";
+import { AppProps, AppType } from 'next/app'; // Import AppProps for better type support
+import { withTRPC } from '@trpc/next';
+// import superjson from 'superjson'; // If you use superjson for serialization
 
-import AppRouter  from "./api/trpc/[trpc]";
-import "../styles/globals.css";
+import AppRouter from './api/trpc/[trpc]';
+import '../styles/globals.css';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+// Define your MyApp component
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
 
+// Function to determine the base URL for the tRPC client
 const getBaseUrl = () => {
-  if (process.browser) return "";
-  if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-
+  if (typeof window !== 'undefined') {
+    // Browser should return an empty string
+    return '';
+  }
+  // If running on Vercel, use the environment variable
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  // Fallback to localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
-export default withTRPC<AppRouter>({
+// Wrap your MyApp with withTRPC
+export default withTRPC<typeof AppRouter>({
   config({ ctx }) {
-    const url = `${getBaseUrl()}/api/trpc`;
-
     return {
-      url,
+      url: `${getBaseUrl()}/api/trpc`,
+      // Optional: if you use superjson for serialization
+      // transformer: superjson,
     };
   },
+  // Enable SSR if needed, set to true if you want to fetch data on the server
   ssr: false,
 })(MyApp);
