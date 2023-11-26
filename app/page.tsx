12305f77a13eab5a621cf8087368b1d443a8d038
Simran Gangwani <simran.gangwani2018@vitstudent.ac.in>
2023-11-26 16:53:34 +0000

    adding changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,29 @@
-// import React from 'react';
-// import FilterablePokedexTable from './components/FilterablePokedexTable';
+import React from 'react';
+import FilterablePokedexTable from './components/FilterablePokedexTable';
 // import { trpc } from './servies'; // Assuming you have a trpc utility set up for tRPC hooks
 
-// export interface PageProps {
-//   params?: any;
-//   searchParams?: any;
-// }
+export interface PageProps {
+  params?: any;
+  searchParams?: any;
+}
 
-// const Page: React.FC<PageProps> = ({ params, searchParams }) => {
-//   const { data: pokemons, isLoading } = trpc.useQuery(['getAllPokemon']); // Replace 'getAllPokemon' with your actual tRPC query
+const Page: React.FC<PageProps> = ({ params, searchParams }) => {
+  const { data: pokemons, isLoading } = trpc.useQuery(['getAllPokemon']); // Replace 'getAllPokemon' with your actual tRPC query
 
-//   if (isLoading) {
-//     return <div>Loading...</div>; // Or any loading component you prefer
-//   }
+  if (isLoading) {
+    return <div>Loading...</div>; // Or any loading component you prefer
+  }
 
-//   if (!pokemons) {
-//     return <div>No Pokémon found.</div>;
-//   }
+  if (!pokemons) {
+    return <div>No Pokémon found.</div>;
+  }
 
-//   return (
-//     <div>
-//       <h1>Welcome to the Pokedex</h1>
-//       {/* <FilterablePokedexTable pokemons={pokemons} /> */}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Welcome to the Pokedex</h1>
+      {/* <FilterablePokedexTable pokemons={pokemons} /> */}
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
